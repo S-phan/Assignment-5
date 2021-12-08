09763926e9b5b102d7da9840ae6cfd898dc91016
S-phan <stphan0001@gmail.com>
2021-12-08 05:08:59 +0000

    sh script file

--- text_binary_classify.sh
+++ text_binary_classify.sh
@@ -2,6 +2,11 @@
 
 
 java weka.core.converters.TextDirectoryLoader -dir $1
+
+# convert to arff 
+
 java weka.core.converters.TextDirectoryLoader -dir $1 > $2
+
+# word to vector
 java -Xmx1024m weka.filters.unsupervised.attribute.StringToWordVector  -i $2 -o $3 -M 2
 java -Xmx1024m  weka.classifiers.meta.ClassificationViaRegression -W weka.classifiers.trees.M5P -num-decimal-places 4  -t  $3
