ac570a6bb9a0f4bd1341ca6e015d1d0129f0eda8
S-phan <stphan0001@gmail.com>
2021-12-08 05:01:26 +0000

    other half of Assignment5

---
+++ text_binary_classify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+java weka.core.converters.TextDirectoryLoader -dir $1
+java weka.core.converters.TextDirectoryLoader -dir $1 > $2
+java -Xmx1024m weka.filters.unsupervised.attribute.StringToWordVector  -i $2 -o $3 -M 2
+java -Xmx1024m  weka.classifiers.meta.ClassificationViaRegression -W weka.classifiers.trees.M5P -num-decimal-places 4  -t  $3
